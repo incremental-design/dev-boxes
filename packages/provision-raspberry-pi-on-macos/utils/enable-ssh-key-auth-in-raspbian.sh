fa68acad57d73ba0c3991abe2be71558714e3f8b
Ajay Ganapathy <lets.talk@designbyajay.com>
2020-10-11 11:42:31 +0000

    expect script to shell into pi now works

--- packages/provision-raspberry-pi-on-macos/utils/enable-ssh-key-auth-in-raspbian.sh
+++ packages/provision-raspberry-pi-on-macos/utils/enable-ssh-key-auth-in-raspbian.sh
@@ -3,7 +3,9 @@
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthenication yes/' /etc/ssh/sshd_config
 sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
 sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config 
+# sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config 
+
+echo 'pubkey auth enabled'
 
 mkdir /home/pi/.ssh
 chmod 700 /home/pi/.ssh
@@ -13,4 +15,8 @@ mv /boot/authorized_keys /home/pi/.ssh/authorized_keys
 chmod 644 /home/pi/.ssh/authorized_keys
 chown pi /home/pi/.ssh/authorized_keys
 
-service ssh restart
+echo 'your pubkey has been added to the image. You can now log in with key based authentication'
+
+service ssh reload
+
+echo 'ssh reloaded'
