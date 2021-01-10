602668232fa10ccc952cf7eb14ce23cf38b02e85
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-10 13:36:09 -0500

    fix malformed API request
    `do-wrapper`’s types insist that the `rancherInstance` have certain properties set with values of certain types. Unfortunately, the digitalOcean api does not actually accept these properties, and throws a `422 unprocessable entity` error. To fix, I added a `@ts-ignore` annotation to `digitalOceanWrapper.droplets.create(…)` call, so that I could override the `do-wrapper` types and send the correct request to digitalOcean

--- packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/index.ts
+++ packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/index.ts
@@ -3,23 +3,23 @@ import getAuthToken from "./getDigitalOceanPersonalAccessToken";
 
 export default function createRancherInstance(): void {
   let rancherInstance = {
-    name: "test",
+    name: "another",
     region: "nyc3",
     size: "s-1vcpu-1gb",
     image: "rancheros",
     ssh_keys: [27608986, 28496457],
     backups: false,
     ipv6: true,
-    private_networking: true,
-    monitoring: true,
-    user_data: "",
-    volumes: [""],
-    tags: "",
+    private_networking: null,
+    user_data: null,
+    volumes: null,
+    tags: ["web"],
   };
 
   const authToken = getAuthToken();
   const digitalOceanWrapper = new DigitalOcean(authToken);
   digitalOceanWrapper.droplets
+    // @ts-ignore: types for rancherInstance are not compatible
     .create(rancherInstance)
     .then((data) => console.log(data))
     .catch((error) => console.error(error));
