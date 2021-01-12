a572cf6257b40e486e668bf8316b7f2135d84204
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-12 08:52:57 -0500

    convert digital ocean api to swagger 2

--- packages/ansible-setup-owncloud/utils/get-digitalocean-api/index.ts
+++ packages/ansible-setup-owncloud/utils/get-digitalocean-api/index.ts
@@ -1,1 +1,26 @@
 import converter from "api-spec-converter";
+import swaggerParser from "@apidevtools/swagger-parser";
+import fs from "fs";
+
+const digitalOceanAPI = {
+  from: "openapi_3",
+  to: "swagger_2",
+  source:
+    "https://api-engineering.nyc3.digitaloceanspaces.com/spec-ci/DigitalOcean-public.v2.yaml",
+};
+const api = converter
+  .convert(digitalOceanAPI)
+  .then(function (convertedAPI: any) {
+    swaggerParser
+      .dereference(convertedAPI)
+      .then(function (dereferencedAPI: any) {
+        JSON.stringify(dereferencedAPI);
+      });
+  });
+
+const date = new Date();
+const today = date.toISOString().split("T")[0];
+
+fs.writeFile("digitalOceanAPI-" + today + ".json", api, "utf8", (error) => {
+  console.log(error);
+});
