f075728144d7fd76ea06b7ad0627932ae385058b
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-12 11:27:23 -0500

    configure webpack in `utils.get-digitalocean-api`
    1. duplicate `webpack.config.js` to `utils/get-digitalocean-api/webpack.config.js`
    2. `yarn add —dev webpack-node-externals` see: https://www.npmjs.com/package/webpack-node-externals
    3.  `require(‘webpack-node-externals’)` in `utils/get-digitalocean-api/webpack.config.js`
    4. set `externals: [nodeExternals()]` in `utils/get-digitalocean-api/webpack.config.js`
    5. make `nodeExternalsConfig` object that points to the `node_modules` folder in the project root and forces commonjs module imports
    6. use `modules: ['../../node_modules’]` to point webpack to the node modules folder in the project root

---
+++ packages/ansible-setup-owncloud/utils/get-digitalocean-api/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const nodeExternalsConfig = {
+  importType: "commonjs",
+  modulesDir: "../../node_modules",
+};
+
+module.exports = {
+  entry: "./index.ts",
+  devtool: "inline-source-map",
+  target: "node",
+  externals: [nodeExternals(nodeExternalsConfig)],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  modules: ["../../node_modules"],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      util: "util",
+    },
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname),
+  },
+};
