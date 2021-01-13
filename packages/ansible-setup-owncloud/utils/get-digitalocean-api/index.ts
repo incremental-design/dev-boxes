01a1e9c30650d98599cf7815a3b47535f5740f02
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-13 08:24:02 -0500

    fix unresolved promise in `utils/get-digitalocea…`
    fix unresolved promise in `utils/get-digitalocean-api/index.js`
    
    note: I had to `yarn add tmp-promise` so that I could create a temporary file to store intermediate parsing results in.

--- packages/ansible-setup-owncloud/utils/get-digitalocean-api/index.ts
+++ packages/ansible-setup-owncloud/utils/get-digitalocean-api/index.ts
@@ -1,26 +1,38 @@
 import converter from "api-spec-converter";
 import swaggerParser from "@apidevtools/swagger-parser";
+import { file } from "tmp-promise";
 import fs from "fs";
 
-const digitalOceanAPI = {
-  from: "openapi_3",
-  to: "swagger_2",
-  source:
-    "https://api-engineering.nyc3.digitaloceanspaces.com/spec-ci/DigitalOcean-public.v2.yaml",
+const apiSource =
+  "https://api-engineering.nyc3.digitaloceanspaces.com/spec-ci/DigitalOcean-public.v2.yaml";
+
+const generateAPI = async function () {
+  try {
+    const dereferencedAPI = await swaggerParser.dereference(apiSource);
+    const dereferencedAPIJSON = JSON.stringify(dereferencedAPI);
+    const { path, cleanup } = await file();
+    fs.writeFileSync(path, dereferencedAPIJSON); //FIXME: synchronously writing a file slows down the whole program.
+    const digitalOceanAPI = {
+      from: "openapi_3",
+      to: "swagger_2",
+      source: path,
+    };
+    const convertedAPI: object = await converter.convert(digitalOceanAPI);
+    cleanup();
+    const convertedAPIJSON = JSON.stringify(convertedAPI);
+    const date = new Date();
+    const today = date.toISOString().split("T")[0];
+    fs.writeFile(
+      "digitalOceanAPI-" + today + ".json",
+      convertedAPIJSON,
+      "utf8",
+      (error) => {
+        console.log(error);
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
 };
-const api = converter
-  .convert(digitalOceanAPI)
-  .then(function (convertedAPI: any) {
-    swaggerParser
-      .dereference(convertedAPI)
-      .then(function (dereferencedAPI: any) {
-        JSON.stringify(dereferencedAPI);
-      });
-  });
 
-const date = new Date();
-const today = date.toISOString().split("T")[0];
-
-fs.writeFile("digitalOceanAPI-" + today + ".json", api, "utf8", (error) => {
-  console.log(error);
-});
+generateAPI();
