c096bcac9c4f05a9b81c4dbad3e518d428de339d
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-17 18:33:25 -0500

    poll for IP addresses
    digital ocean doesn’t actually give a droplet an IP address until about 30 seconds after it is created. So, we have to keep polling until we get an IP address.
    
    The polling loop continues until either the maximum number of seconds to poll has elapsed, or an ipv4 address has been found. If the droplet supports ipv6, the polling loop continues until either the maximum number of seconds to poll has elapsed, or both an ipv4 and ipv6 address are found.

--- packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/RancherOSConfig.ts
+++ packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/RancherOSConfig.ts
@@ -1,13 +1,17 @@
 import {
   provisionOnDigitalOcean,
   digitalOceanCreateDropletResponse,
+  getDropletInformation,
 } from "./provisionOnDigitalOcean";
 import { provisionOnAWS } from "./provisionOnAWS";
+import { inspect } from "util";
 
 export class RancherOSConfig {
   // !Private Members
   private _name: string = "";
   private _ipv4Addresses: Set<string> = new Set();
+  private _ipv6Addresses: Set<string> = new Set();
+  private _dropletID: string = "";
 
   // !Constructor
   constructor() {} //I'm leaving the constructor blank because we'll initialize all variables with chainable setters
@@ -27,9 +31,25 @@ export class RancherOSConfig {
   }
 
   set ipv4Addresses(ipv4Addresses: Array<string>) {
-    this._ipv4Addresses = new Set(ipv4Addresses);
+    this._ipv6Addresses = new Set(ipv4Addresses);
     //TODO: iterate through the set and remove any ip addresses that are not valid.
   }
+
+  get ipv6Addresses(): Array<string> {
+    return Array.from(this._ipv6Addresses);
+  }
+
+  set ipv6Addresses(ipv4Addresses: Array<string>) {
+    this._ipv6Addresses = new Set(ipv4Addresses);
+    //TODO: iterate through the set and remove any ip addresses that are not valid.
+  }
+
+  get dropletID(): string {
+    return this._dropletID;
+  }
+  /*
+    note that there is no setter for dropletID, and the _dropletID property is private. This is because we do NOT want the dropletID to be set by any code outside of this class.
+  */
 
   // !Accessor Methods
   setName(newName: string): this {
@@ -50,18 +70,100 @@ export class RancherOSConfig {
     return this;
   }
 
+  addIpv6Address(ipv6Address: string): this {
+    let addressArray = this.ipv6Addresses;
+    addressArray.push(ipv6Address);
+    return this;
+  }
+
+  removeIpv6Address(ipv6Address: string): this {
+    this.ipv6Addresses = this.ipv6Addresses.filter(
+      (address) => address !== ipv6Address
+    );
+    return this;
+  }
+
   // !Subroutines (these should be private)
   private parseDigitalOceanDropletResponse(
     response: digitalOceanCreateDropletResponse
   ): void {
-    // TODO: grab all relevant values and use them to set rancherOS config
-    response.networks.v4.forEach((item) => {
-      if (item.type === "public") {
-        this.addIpv4Address(item.ip_address);
-        console.log(item, "logged");
+    try {
+      response.droplet.networks.v4.forEach((item) => {
+        if (item.type === "public") {
+          this.addIpv4Address(item.ip_address);
+          console.log(item, "logged");
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    this._dropletID = response.droplet.id;
+  }
+
+  private pollForIPAddresses(maximumNumberOfSecondsToPoll: number = 15): void {
+    /*
+      This function really should not be necessary. Polling is generally a bad idea. However, provisionOnDigitalOcean.droplets.create() doesn't actually return the droplet's IP address. This is because it takes ~5-10 seconds for the droplet to actually acquire an IP address. So, in this particular case, we need to poll until we get an IP address.
+      
+      In the first poll, we check response.features to see if it contains "ipv6". If so, we continue to poll until we have received at least 1 ipv4 and 1 ipv6 address, or until the number of seconds to poll is up.
+      
+      If the number of seconds to poll is less than zero, then we will poll indefinitely until we get at least 1 ipv4 address, and if ipv6 is enabled, 1 ipv6 address
+    */
+    const pollIndefinitely: boolean =
+      maximumNumberOfSecondsToPoll < 0 ? true : false;
+    let shouldPoll: boolean = true;
+    let dropletHasIpv6: boolean;
+    let ipv4Addresses: Array<any> = [];
+    let ipv6Addresses: Array<any> = [];
+
+    if (!pollIndefinitely) {
+      setTimeout(() => {
+        shouldPoll = false;
+      }, maximumNumberOfSecondsToPoll * 1000);
+    }
+
+    const pollDigitalOceanForIPAddresses = async () => {
+      while (shouldPoll) {
+        console.log(`polling ${this.dropletID} for IP addresses`);
+        let response: digitalOceanCreateDropletResponse = await getDropletInformation(
+          this.dropletID
+        );
+        if (dropletHasIpv6 === undefined) {
+          console.log(inspect(response, false, null, true));
+          if (response.droplet.features.includes("ipv6")) {
+            dropletHasIpv6 = true;
+          } else {
+            dropletHasIpv6 = false;
+          }
+        }
+        if (ipv4Addresses.length == 0) {
+          ipv4Addresses = response.droplet.networks.v4;
+        }
+        if (dropletHasIpv6 && ipv6Addresses.length == 0) {
+          ipv6Addresses = response.droplet.networks.v6;
+        }
+        if (ipv4Addresses.length > 0) {
+          if (ipv6Addresses.length > 0 || !dropletHasIpv6) {
+            break;
+          }
+        }
       }
-    });
-    return;
+    };
+    pollDigitalOceanForIPAddresses();
+
+    if (ipv4Addresses.length > 0) {
+      ipv4Addresses.forEach((address) => {
+        if (address.type === "public") {
+          this.addIpv4Address(address.ip_address);
+        }
+      });
+    }
+    if (ipv6Addresses.length > 0) {
+      ipv6Addresses.forEach((address) => {
+        if (address.type === "public") {
+          this.addIpv6Address(address.ip_address);
+        }
+      });
+    }
   }
 
   async provisionOn(cloud: cloudProviders): Promise<this> {
@@ -77,6 +179,7 @@ export class RancherOSConfig {
           config
         );
         this.parseDigitalOceanDropletResponse(response);
+        this.pollForIPAddresses(); //note that we need to poll for ip addresses until we receive them because response does not actually contain IP addresses.
         return this;
       case cloudProviders.aws:
         await provisionOnAWS();
