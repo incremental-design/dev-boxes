2ecc1a8313dce0c07ce11adcd3abc1c7183608c2
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-17 17:55:34 -0500

    fix type guard behavior
    type guards (see: https://www.typescriptlang.org/docs/handbook/advanced-types.html ) work super if you aren’t comparing a type to ‘undefined’. In this case, we are. `authToken` can be either a string or undefined. It is undefined if `DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN` is not set in `.env`. Changing the type guard from a custom type guard to `typeof authToken …` (line 7) keeps the type guard from erroring out when the auth token is undefined.

--- packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/getDigitalOceanPersonalAccessToken.ts
+++ packages/ansible-setup-owncloud/src/provisionRancherOnDigitalOcean/getDigitalOceanPersonalAccessToken.ts
@@ -1,16 +1,14 @@
-export default function (): string {
-  function personalAccessTokenIsUndefined(
-    personalAccessToken: string | undefined
-  ): personalAccessToken is undefined {
-    return (personalAccessToken as string).length == undefined;
-  }
 import dotenv from "dotenv";
 dotenv.config(); // we have to initialize environment vars before we can actually access auth token
 
+export default function (): string {
   const authToken = process.env.DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN;
 
-  if (personalAccessTokenIsUndefined(authToken)) {
-    throw "Digital Ocean Personal Access Token has not been set in `.env`. Cannot access digital ocean API";
+  if (typeof authToken === "string") {
+    return authToken;
+  } else {
+    throw new Error(
+      "Digital Ocean Personal Access Token has not been set in `.env`. Cannot access digital ocean API"
+    );
   }
-  return authToken;
 }
