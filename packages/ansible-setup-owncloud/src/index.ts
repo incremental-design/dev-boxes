02608fd54c341d6d7f2956215db22b109f37f71a
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-01-23 10:18:07 -0500

    generate random passwords for owncloud
    in `src/index.ts` generate 3 random passwords for the owncloud admin user, the mariadb root user and the mariadb owncloud user. allow the person running the program to change these passwords to whatever they want

--- packages/ansible-setup-owncloud/src/index.ts
+++ packages/ansible-setup-owncloud/src/index.ts
@@ -13,13 +13,41 @@ import chalk from "chalk";
 import terminalLink from "terminal-link";
 import { execSync } from "child_process";
 import { resolve, relative } from "path";
+import passwordGenerator from "generate-password";
 
 const cli = cac();
 const parsed = cli.parse();
+function makeRandomPasswords(): {
+  adminPassword: string;
+  mariaDBRootPassword: string;
+  mariaDBPassword: string;
+} {
+  const passwordArray = passwordGenerator.generateMultiple(3, {
+    length: 16,
+    symbols: true,
+    numbers: true,
+    lowercase: true,
+    uppercase: true,
+    strict: true,
+    exclude: "\"$'\\",
+  });
+  return {
+    adminPassword: passwordArray[0],
+    mariaDBRootPassword: passwordArray[1],
+    mariaDBPassword: passwordArray[2],
+  };
+}
 
 let DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN: string = "";
 
-function getQuestions(token: string): Array<prompts.PromptObject> {
+function getQuestions(
+  token: string,
+  passwords: {
+    adminPassword: string;
+    mariaDBRootPassword: string;
+    mariaDBPassword: string;
+  }
+): Array<prompts.PromptObject> {
   return [
     {
       type: token ? "confirm" : false,
@@ -49,6 +77,24 @@ function getQuestions(token: string): Array<prompts.PromptObject> {
       message: "What do you want to name your droplet?",
       initial: "owncloud",
     },
+    {
+      type: "text",
+      name: "adminPassword",
+      message: "What password do you want to use to log into owncloud?",
+      initial: passwords.adminPassword,
+    },
+    {
+      type: "text",
+      name: "mariaDBRootPassword",
+      message: "What password do you want the MariaDB root user to have?",
+      initial: passwords.mariaDBRootPassword,
+    },
+    {
+      type: "text",
+      name: "mariaDBPassword",
+      message: "What password do you want the MariaDB owncloud user to have?",
+      initial: passwords.mariaDBPassword,
+    },
   ];
 }
 
@@ -59,10 +105,13 @@ function ansibleSetupRancher(
   pathToInventory: string,
   pathToPlaybook: string,
   pathToDockerCompose: string,
-  pathToEnv: string
+  pathToEnv: string,
+  adminPassword: string,
+  mariaDBRootPassword: string,
+  mariaDBPassword: string
 ) {
   return execSync(
-    `ansible-playbook '${pathToPlaybook}' --extra-vars='rancher_ip=${ipAddress} docker_compose=${pathToDockerCompose} env=${pathToEnv}' --inventory='${pathToInventory}'`,
+    `ansible-playbook '${pathToPlaybook}' --extra-vars='rancher_ip=${ipAddress} docker_compose=${pathToDockerCompose} env=${pathToEnv} admin_password=${adminPassword} maria_db_root_password=${mariaDBRootPassword} maria_db_password=${mariaDBPassword}' --inventory='${pathToInventory}'`,
     { stdio: "inherit" }
   );
 }
@@ -85,8 +134,9 @@ const playbook = relative(
 
 (async () => {
   DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN = await getDigitalOceanPersonalAccessToken();
+  const passwords = makeRandomPasswords();
   const answers = await prompts(
-    getQuestions(DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN)
+    getQuestions(DIGITAL_OCEAN_PERSONAL_ACCESS_TOKEN, passwords)
   );
   if (answers.newToken ?? undefined) {
     console.log(answers.newToken);
@@ -97,5 +147,14 @@ const playbook = relative(
     .setName(answers.dropletName)
     .provisionOn(cloudProviders.digitalOcean);
   const ipAddress = droplet.ipv4Addresses.slice(-1)[0];
-  ansibleSetupRancher(ipAddress, inventory, playbook, dockerCompose, env);
+  ansibleSetupRancher(
+    ipAddress,
+    inventory,
+    playbook,
+    dockerCompose,
+    env,
+    answers.adminPassword,
+    answers.mariaDBRootPassword,
+    answers.mariaDBPassword
+  );
 })();
