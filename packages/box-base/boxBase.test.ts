eaf7f14003e592bd5da34ec6fcea3dd9b402cbfb
Ajay Ganapathy <ajay@incremental.design>
2022-02-10 15:41:40 -0500

    make a `generateCLI` function that makes a CLI with both an interactive and non-interactive mode
    I also stubbed a jest test ... it doesn't test prompts correctly right now.

--- packages/box-base/boxBase.test.ts
+++ packages/box-base/boxBase.test.ts
@@ -1,10 +1,58 @@
-import { generatePasswords } from './src/Quickstart';
+import { generatePasswords, getAnswersFromCLI } from './src/Quickstart';
+import { PromptType } from 'prompts';
+jest.unmock('mock-stdin');
 
 describe('generatePasswords', () => {
   it('should generate 100 passwords', () => {
+    let passwords = '';
     for (const password of generatePasswords()) {
-      console.log(password);
       expect(password).toHaveLength(36);
+      passwords += `${password}\n`;
     }
+    console.log(passwords);
+  });
+});
+
+describe('getAnswersFromCLI', () => {
+  const samplePrompts: Array<{
+    name: string;
+    message: string;
+    type: PromptType;
+    initial: string;
+  }> = [
+    {
+      name: 'firstname',
+      message: 'What is your first name?',
+      type: 'text',
+      initial: 'Ajay',
+    },
+    // {
+    //   name: 'lastname',
+    //   message: 'What is your last name?',
+    //   type: 'text',
+    //   initial: 'Ganapathy',
+    // },
+    // {
+    //   name: 'hobby',
+    //   message: 'What is your hobby?',
+    //   type: 'text',
+    //   initial: 'running',
+    // },
+  ];
+
+  let stdin;
+  beforeEach(() => {
+    stdin = require('mock-stdin').stdin();
+  });
+  it('should receive ', async () => {
+    let i = 0;
+    const send = () => {
+      process.nextTick(stdin.send('abcd\r'));
+      // todo: figure out how to wait for prompt before sending next response
+    };
+    send();
+    const results = await getAnswersFromCLI(samplePrompts);
+    console.log(results);
+    // console.log('alldata', alldata);
   });
 });
