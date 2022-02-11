a48adb415cc4c8fece7af6d0975beb5812e35aa8
Ajay Ganapathy <lets.talk@designbyajay.com>
2022-02-11 07:22:50 -0500

    make a `makePasswordPrompt` function

--- packages/box-base/src/quickstart.ts
+++ packages/box-base/src/quickstart.ts
@@ -1,7 +1,8 @@
 import passgen from 'generate-password';
 import cac from 'cac';
-import prompts from 'prompts';
+import prompts, { PromptObject } from 'prompts';
 import keytar from 'keytar';
+import { text } from 'stream/consumers';
 
 function quickstart(): void {
   // your code here
@@ -9,15 +10,21 @@ function quickstart(): void {
 }
 
 /**
- * generatePasswords makes 100 random passwords.
+ * generatePasswords makes random passwords.
  *
  * @param length - number of characters in password. Defaults to 36 characters.
  * @param symbols - whether to include special characters in the password. Defaults to true.
  *
- * @returns an iterator that returns 100 uniques string that you can use as a password.
+ * @param numberToGenerate - the number of passwords to generate. Defaults to 100.
+ *
+ * @returns an iterator that returns unique string that you can use as a password.
  */
-export function* generatePasswords(length = 36, symbols = true) {
-  const PA = passgen.generateMultiple(100, {
+export function* generatePasswords(
+  length = 36,
+  symbols = true,
+  numberToGenerate = 100
+): IterableIterator<string> {
+  const PA = passgen.generateMultiple(numberToGenerate, {
     length,
     symbols,
     numbers: true,
@@ -92,11 +99,65 @@ export async function addToKeychain(
   return keytar.setPassword(service, account, password);
 }
 
+/**
+ * retrieveFromKeychain gets a password from the keychain.
+ * @param service
+ * @param account
+ * @returns a password, or null if the password is not found.
+ */
 export async function retrieveFromKeychain(
   service: string,
   account: string
 ): Promise<string | null> {
   return keytar.getPassword(service, account);
+}
+
+/**
+ * makePasswordPrompt returns an array of {@link PromptObject}s that ask a user to set a password.
+ * @param service - the service that the account is associated with.
+ * @param account - the account who's password you want to update.
+ * @param newPassword - the password you want to update to.
+ * @returns array of prompts that ask the user if they want to update the password, and if so, what they want to update to.
+ * 
+ * @remarks
+ * These prompts only show up if:
+ *  - the user has never set a password for the given service and account, and hasn't passed the --autoPassword flag.
+ *  - the user passes the --changePassword flag into the CLI.
+ * Note that these prompts DON'T ACTUALLY SET THE PASSWORD. They only store the password to set in the `newPassword${service}${account}` prompts answers object. It is up to you to actually take the password and add it to the keychain, with {@link addToKeychain}.
+ */
+export async function makePasswordPrompt(
+  service: string,
+  account: string,
+  newPassword?: string
+): Promise<Array<PromptObject> {
+  const password = await retrieveFromKeychain(service, account);
+  const np = newPassword || generatePasswords(36, true, 1).next().value;
+  if (password)
+    return [
+        {
+          name: `changePassword${service}${account}`,
+          message: `Your current password for ${service}: ${account} is ${password}. Do you want to change it?`,
+          type: (prev: any) =>
+            process.argv.includes('--changePassword')
+              ? 'confirm'
+              : false /* you HAVE to pass the --changePassword flag in order to make this prompt show up */,
+          initial: false,
+        },
+        {
+          name: `newPassword${service}${account}`,
+          message: 'What is your new password?',
+          type: (prev) => prev ? 'text' : false,
+          initial: np,
+        },
+      ]
+  return [
+    {
+      name: `newPassword${service}${account}`,
+      message: `Choose a password for ${service}: ${account}`,
+      type: (prev: any) => process.argv.includes('--autoPassword') ? 'text' : false,
+      initial: np,
+    }
+  ]
 }
 
 export default quickstart;
